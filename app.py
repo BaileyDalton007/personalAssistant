7abd74fdab2e989817a7e840e7307b671315fb12
Bailey Dalton <baileydalton007@gmail.com>
2022-02-20 19:35:39 -0500

    Speech recognition energy threshold

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 import speech_recognition as sr
 
+name = 'arnold'
+
 listener = sr.Recognizer()
+listener.energy_threshold = 105 # use python -m speech_recognition
+listener.dynamic_energy_threshold = True
 
 try:
     with sr.Microphone() as source:
+        listener.adjust_for_ambient_noise(source)
         print("listening ..")
         voice = listener.listen(source)
         command = listener.recognize_google(voice)
-        print(command)
+        command = command.lower()
+        if name in command:
+            print(command)
 except:
     pass
